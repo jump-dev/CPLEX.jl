d30701237aa92544ecedfa2619ffd4f54a9daa56
Joey Huchette <joehuchette@gmail.com>
2013-11-10 18:36:08 -0500

    uncomment full tests

--- test/mathprog.jl
+++ test/mathprog.jl
@@ -1,10 +1,10 @@
 using Cplex
 
-# include(joinpath(Pkg.dir("MathProgBase"),"test","linprog.jl"))
-# linprogtest(CplexSolver())
+include(joinpath(Pkg.dir("MathProgBase"),"test","linprog.jl"))
+linprogtest(CplexSolver())
 
-# include(joinpath(Pkg.dir("MathProgBase"),"test","mixintprog.jl"))
-# mixintprogtest(CplexSolver())
+include(joinpath(Pkg.dir("MathProgBase"),"test","mixintprog.jl"))
+mixintprogtest(CplexSolver())
 
 include(joinpath(Pkg.dir("MathProgBase"),"test","linproginterface.jl"))
 linprogsolvertest(CplexSolver())
