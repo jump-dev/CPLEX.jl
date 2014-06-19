377beb0cb9220e16a278ddfcc57752449571b147
Joey Huchette <joehuchette@gmail.com>
2014-06-18 21:12:02 -0500

    make continuous SOS work

--- src/cpx_constrs.jl
+++ src/cpx_constrs.jl
@@ -344,4 +344,6 @@ function add_sos!(model::Model, sostype::Symbol, idx::Vector{Int}, weight::Vecto
     if stat != 0
         throw(CplexError(model.env, stat))
     end
+    nelem > 0 && (model.has_int = true)
+    return nothing
 end
