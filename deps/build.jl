74b9f9a69ff1ed1b458b92d9f1677a25546ecc31
yeesian <ngyeesian@gmail.com>
2014-06-19 23:48:53 +0800

    (yet) another library version

--- deps/build.jl
+++ deps/build.jl
@@ -3,6 +3,6 @@ using BinDeps
 
 @osx_only dlopen("libstdc++",RTLD_GLOBAL)
 
-libcplex = library_dependency("libcplex",aliases=["libcplex124.so","cplex124","cplex1251","libcplex1251.so", "libcplex125.dylib","libcplex1251.dylib","libcplex1251.jnilib","libcplex1260.dylib"])
+libcplex = library_dependency("libcplex",aliases=["libcplex124.so","cplex124","libcplex125.so","cplex125","cplex1251","libcplex1251.so", "libcplex125.dylib","libcplex1251.dylib","libcplex1251.jnilib","libcplex1260.dylib"])
 
 @BinDeps.install [ :libcplex => :libcplex ]
