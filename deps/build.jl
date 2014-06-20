a0003fcab963a47c2e9c3754040556f336889543
Miles Lubin <miles.lubin@gmail.com>
2014-06-20 11:54:16 -0600

    untested code for finding CPLEX on windows

--- deps/build.jl
+++ deps/build.jl
@@ -3,6 +3,19 @@ using BinDeps
 
 @osx_only dlopen("libstdc++",RTLD_GLOBAL)
 
-libcplex = library_dependency("libcplex",aliases=["libcplex124.so","cplex124","libcplex125.so","cplex125","cplex1251","libcplex1251.so", "libcplex125.dylib","libcplex1251.dylib","libcplex1251.jnilib","libcplex1260.dylib"])
+libcplex = library_dependency("libcplex",aliases=["libcplex124.so","cplex124","libcplex125.so","cplex125","cplex1251","libcplex1251.so", "libcplex125.dylib","libcplex1251.dylib","libcplex1251.jnilib","cplex1260","libcplex1260.dylib"])
+
+@windows_only begin
+    cpxvers = ["126"]
+    for v in cpxvers
+        env = "CPLEX_STUDIO_BINARIES$v"
+        if haskey(ENV,env)
+            for d in split(ENV[env],';')
+                provides(Binaries, d, libcplex)
+            end
+        end
+    end
+end
+
 
 @BinDeps.install [ :libcplex => :libcplex ]
