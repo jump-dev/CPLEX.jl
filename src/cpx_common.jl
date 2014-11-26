fb6e4905f16095dfef6b02570fa107070f00d114
Joey Huchette <joehuchette@gmail.com>
2014-11-26 15:53:26 -0500

    hopefully fix constraint dual sign (#23), and various compat fixes

--- src/cpx_common.jl
+++ src/cpx_common.jl
@@ -29,9 +29,9 @@ ivec(v::IVec) = v
 fvec(v::FVec) = v
 cvec(v::CVec) = v
 
-ivec{I<:Integer}(v::Vector{I}) = convert(IVec, v)
-fvec{T<:Real}(v::Vector{T}) = convert(FVec, v)
-cvec(v::Vector{Char}) = convert(CVec, v)
+ivec(v::Vector) = convert(IVec, v)
+fvec(v::Vector) = convert(FVec, v)
+cvec(v::Vector) = convert(CVec, v)
 
 # cvecx(v, n) and fvecx(v, n)
 # converts v into a vector of Cchar or Float64 of length n,
