9cf3914c3e48e88653fbea0fe83e0384d6297f11
Joey Huchette <joehuchette@gmail.com>
2015-12-30 19:34:04 -0500

    Close #62

--- deps/build.jl
+++ deps/build.jl
@@ -3,7 +3,7 @@ using BinDeps, Compat
 
 @osx_only Libdl.dlopen("libstdc++",Libdl.RTLD_GLOBAL)
 
-cpxvers = ["124","125","1251","1260","1261","1262"]
+cpxvers = ["124","125","1251","1260","1261","1262","1263"]
 
 libnames = ASCIIString["cplex"]
 for v in cpxvers
@@ -15,7 +15,7 @@ end
 libcplex = library_dependency("libcplex",aliases=libnames)
 
 @windows_only begin
-    wincpxvers = ["126","1261","1262"]
+    wincpxvers = ["126","1261","1262","1263"]
     for v in wincpxvers
         env = "CPLEX_STUDIO_BINARIES$v"
         if haskey(ENV,env)
