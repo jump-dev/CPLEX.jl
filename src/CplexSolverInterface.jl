c4fa7664ded9b84b4347b23306c7ef9a0ae424e9
odow <odow003@aucklanduni.ac.nz>
2016-09-29 08:54:44 +1300

    Implement solver independent parameters

--- src/CplexSolverInterface.jl
+++ src/CplexSolverInterface.jl
@@ -17,7 +17,15 @@ function CplexMathProgModel(;options...)
     # set_param!(env, "CPX_PARAM_PRELINEAR", 0) # MAY NOT BE NECESSARY, only performs linear presolving so can recover original variables
     set_param!(env, "CPX_PARAM_SCRIND", 1) # output logs to stdout by default
     for (name,value) in options
-        set_param!(env, string(name), value)
+        if name == :TimeLimit
+            set_param!(env, "CPX_PARAM_TILIM", value)
+        elseif name == :Silent
+            if value == true
+                set_param!(env, "CPX_PARAM_SCRIND", 0)
+            end
+        else
+            set_param!(env, string(name), value)
+        end
     end
 
     m = CplexMathProgModel(_Model(env), nothing, nothing, nothing, nothing, nothing, nothing, NaN)
--- test/mathprog.jl
+++ test/mathprog.jl
@@ -10,8 +10,9 @@ include(joinpath(Pkg.dir("MathProgBase"),"test","quadprog.jl"))
 quadprogtest(CplexSolver())
 socptest(CplexSolver())
 
+solver = CplexSolver(Silent=true, TimeLimit=100.)
 include(joinpath(Pkg.dir("MathProgBase"),"test","linproginterface.jl"))
-linprogsolvertest(CplexSolver())
+linprogsolvertest(solver)
 
 include(joinpath(Pkg.dir("MathProgBase"),"test","conicinterface.jl"))
 coniclineartest(CplexSolver())
