c561266bfa9c8502b25e4571c3fbee5b64854e45
Miles Lubin <miles.lubin@gmail.com>
2016-11-21 11:40:50 -0500

    fix return status when exiting from callbacks

--- src/CplexSolverInterface.jl
+++ src/CplexSolverInterface.jl
@@ -200,7 +200,7 @@ function status(m::CplexMathProgModel)
         Base.warn_once("CPLEX reported infeasible or unbounded. Set CPX_PARAM_REDUCE=1 to check
                         infeasibility or CPX_PARAM_REDUCE=2 to check unboundedness.")
         :InfeasibleOrUnbounded
-    elseif contains(string(ret), "TIME_LIM")
+    elseif contains(string(ret), "TIME_LIM") || contains(string(ret), "MIP_ABORT")
         :UserLimit
     else
         ret
