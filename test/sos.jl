231bc92757dc0dea19b5ba87545c9ae1522931af
odow <o.dowson@gmail.com>
2017-02-25 11:54:46 +1300

    Fix handling of SOS and problem type. Fixes #116

---
+++ test/sos.jl
@@ -0,0 +1,39 @@
+using CPLEX, Base.Test
+
+m = CPLEX.CplexMathProgModel()
+CPLEX.loadproblem!(m,
+    [1 2 3 0 0 0 0 0 -1 0; 0 0 0 5 4 7 2 1 0 -1], # constraint matrix
+    fill(-Inf, 10), # variable lb
+    fill(Inf, 10), # variable ub
+    vcat(zeros(8), ones(2)), # objective vector
+    [0, 0], # constraint lb
+    [0, 0], # constraint ub
+    :Max)
+@test CPLEX.get_prob_type(m.inner) == :LP
+
+CPLEX.setvartype!(m, vcat(fill(:Bin, 8), fill(:Cont, 2)))
+@test CPLEX.get_prob_type(m.inner) == :MILP
+
+CPLEX.addsos1!(m, [1,2,3], [1.0,2.0,3.0])
+CPLEX.addsos2!(m, [4,5,6,7,8], [5.0, 4.0, 7.0, 2.0, 1.0])
+@test CPLEX.get_prob_type(m.inner) == :MILP
+
+CPLEX.optimize!(m)
+
+@test isapprox(CPLEX.getobjval(m), 15.0)
+sol = CPLEX.getsolution(m)
+@test isapprox(sol[9],  3.0)
+@test isapprox(sol[10], 12.0)
+
+# ======================================
+
+m2 = CPLEX.CplexMathProgModel()
+CPLEX.loadproblem!(m2, Array(Float64, (0,3)), [-Inf, -Inf, -Inf], [1,1,2], [2,1,1], Float64[], Float64[], :Max)
+@test CPLEX.get_prob_type(m2.inner) == :LP
+
+CPLEX.addsos1!(m2, [1,2], [1.0,2.0])
+CPLEX.addsos1!(m2, [1,3], [1.0,2.0])
+@test CPLEX.get_prob_type(m2.inner) == :MILP
+
+CPLEX.optimize!(m2)
+@test isapprox(CPLEX.getobjval(m2), 3.0)
