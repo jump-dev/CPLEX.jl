2972d6a742ea7ad199cfbf0597509b4fe047f2bf
joaquimg <joaquimdgarcia@gmail.com>
2017-11-15 20:46:30 -0200

    Set tests back to old ones

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,47 +1,6 @@
 using Base.Test
 # using MathProgBase
 using CPLEX
-using MathOptInterface
-const MOI = MathOptInterface
-
-# @testset "Basic CPLEX Only tests" begin
-#     s = CplexSolver(logfile="abc.txt")
-#     m = MOI.SolverInstance(s)
-# end
-
-include(joinpath(Pkg.dir("MathOptInterface"), "test", "contlinear.jl"))
-
-@testset "LP Related Tests" begin
-    # run all MOI cont linear tests
-    contlineartest(CplexSolver(CPX_PARAM_SCRIND=0, CPX_PARAM_PREIND=0))
-end
-
-include(joinpath(Pkg.dir("MathOptInterface"), "test", "intlinear.jl"))
-
-@testset "MIP Related Tests" begin
-    # run all MOI int linear tests
-    intlineartest(CplexSolver(CPX_PARAM_SCRIND=0))
-end
-
-include(joinpath(Pkg.dir("MathOptInterface"), "test", "contquadratic.jl"))
-
-@testset "QP Related Tests" begin
-    # run all MOI cont quadratic tests
-    contquadratictests(CplexSolver(CPX_PARAM_SCRIND=0), rtol=1e-4)
-end
-
-include(joinpath(Pkg.dir("MathOptInterface"), "test", "contconic.jl"))
-
-@testset "Conic Related Tests" begin
-    # run some MOI cont conic tests
-
-    lin1tests(CplexSolver(CPX_PARAM_SCRIND=0))
-    lin2tests(CplexSolver(CPX_PARAM_SCRIND=0))
-
-    # turn presolve off for certificates
-    lin3test(CplexSolver(CPX_PARAM_SCRIND=0, CPX_PARAM_PREIND=0))
-    lin4test(CplexSolver(CPX_PARAM_SCRIND=0, CPX_PARAM_PREIND=0))
-end
 
 @testset "Legacy Tests" begin
     @testset "Low level" begin
