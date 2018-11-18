d582068a5bff0cdceb2fdb78ded3af7c6b4514da
odow <o.dowson@gmail.com>
2018-11-17 21:07:33 -0600

    Get callbacks working

--- examples/admipex9_cb.jl
+++ examples/admipex9_cb.jl
@@ -1,47 +1,21 @@
-#callbacks for cap problem
-using CPLEX, Revise#, Gallium
-import Base: convert, unsafe_convert, show, copy
-using Compat
-import Gallium
-#revise julia
-# JuliaPath="/Users/chengguo/.julia/v0.6/CPLEX/src/"
-# include("/Users/chengguo/.julia/v0.6/CPLEX/deps/deps.jl")
+using CPLEX
 
-# include(string(JuliaPath,"cpx_common.jl"))
-# include(string(JuliaPath,"cpx_env.jl"))
-# include(string(JuliaPath,"cpx_model.jl"))
-# include(string(JuliaPath,"full_defines_1280.jl"))
-# include(string(JuliaPath,"cpx_params_1280.jl"))
-# include(string(JuliaPath,"cpx_callbacks.jl"))
-# include(string(JuliaPath,"cpx_newcbs.jl"))
-# Libdl.dlopen("libstdc++",Libdl.RTLD_GLOBAL)
-##################
-#example of cplex callable library, admipex9.c
-env=CPLEX.Env()
-#build the model and read the problem from the .mps file
-path=joinpath(dirname(@__FILE__),"sentoy.mps")
-model=CPLEX.Model(env,path)
-CPLEX.read_model(model,path)
-model.has_int = true#need to change cpx_solve.jl L4
-CPLEX.set_param!(env,"CPXPARAM_MIP_Tolerances_MIPGap", 1e-6)
-#set up generic callback
-ncols=CPLEX.num_var(model)
-obj=CPLEX.get_obj(model)
-# where=Clong(0)
-where=Clong(0)
-where |=CPLEX.CPX_CALLBACKCONTEXT_RELAXATION
+env = CPLEX.Env()
+# This is pretty important for thread safety (probably).
+CPLEX.set_param!(env, "CPX_PARAM_THREADS", 1)
+model = CPLEX.Model(env, "callback_test")
+CPLEX.add_vars!(model, [1.0], [0.0], [1.0])
+CPLEX.c_api_chgctype(model, Int32[1], Cchar['I'])
 
-gcbdata=CPLEX.GenCallbackData(ncols,obj)
-# GenCallbackData(gcbdata,ncols,obj)
-# gcbdata.ncol=ncols
-# gcbdata.obj=obj
+# Hack for these tests.
+model.has_int = true
 
-CPLEX.setcallbackfunc(env,model,where,pointer_from_objref(gcbdata))
+function my_callback(cb_context::CPLEX.CallbackContext, context_id::Clong)
+    println("Aborting!")
+    CPLEX.cpx_callbackabort(cb_context)
+end
 
-CPLEX.set_param!(env,"CPXPARAM_MIP_Strategy_HeuristicFreq", -1)
+context_id = Clong(0) | CPLEX.CPX_CALLBACKCONTEXT_CANDIDATE
+CPLEX.cpx_callbacksetfunc(model, context_id, my_callback)
 
-# @enter CPLEX.optimize!(model)
 CPLEX.optimize!(model)
-
-println("The solving status: ", CPLEX.get_status(model))
-println("The optimal objective value is: ", CPLEX.get_objval(model))
