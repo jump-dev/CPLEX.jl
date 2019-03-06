7b3d1f1735b5073926cf1580e083f179e61f0b3e
Mathieu Besançon <mathieu.besancon@gmail.com>
2019-03-06 16:23:42 +0100

    fix Base warning

--- src/CplexSolverInterface.jl
+++ src/CplexSolverInterface.jl
@@ -195,7 +195,7 @@ function MathProgBase.status(m::CplexMathProgModel)
     elseif ret in [:CPX_STAT_INFEASIBLE, :CPXMIP_INFEASIBLE]
         :Infeasible
     elseif ret in [:CPX_STAT_INForUNBD, :CPXMIP_INForUNBD]
-        Base.warn_once("CPLEX reported infeasible or unbounded. Set CPX_PARAM_REDUCE=1 to check
+        @warn("CPLEX reported infeasible or unbounded. Set CPX_PARAM_REDUCE=1 to check
                         infeasibility or CPX_PARAM_REDUCE=2 to check unboundedness.")
         :InfeasibleOrUnbounded
     elseif occursin("TIME_LIM", string(ret)) || occursin("MIP_ABORT", string(ret))
