7edbe45d0c773ccf0b3f27979574b787bdfda3b2
Twan Koolen <koolen.twan@gmail.com>
2019-07-08 10:31:12 -0400

    Fix deprecation warnings due to use of Pkg.dir.

--- test/MathProgBase/mathprog.jl
+++ test/MathProgBase/mathprog.jl
@@ -1,19 +1,23 @@
-include(joinpath(Pkg.dir("MathProgBase"),"test","linprog.jl"))
+import MathProgBase
+
+math_prog_base_test_dir = joinpath(dirname(pathof(MathProgBase)), "..", "test")
+
+include(joinpath(math_prog_base_test_dir, "linprog.jl"))
 linprogtest(CplexSolver(CPX_PARAM_PREIND=0, CPX_PARAM_LPMETHOD=2, CPX_PARAM_SCRIND=0))
 
-include(joinpath(Pkg.dir("MathProgBase"),"test","mixintprog.jl"))
+include(joinpath(math_prog_base_test_dir, "mixintprog.jl"))
 mixintprogtest(CplexSolver(CPX_PARAM_SCRIND=0))
 
-include(joinpath(Pkg.dir("MathProgBase"),"test","quadprog.jl"))
+include(joinpath(math_prog_base_test_dir, "quadprog.jl"))
 quadprogtest(CplexSolver(CPX_PARAM_SCRIND=0))
 socptest(CplexSolver(CPX_PARAM_SCRIND=0))
 
 solver = CplexSolver(CPX_PARAM_SCRIND=0)
 CPLEX.setparameters!(solver, Silent=true, TimeLimit=100.0)
-include(joinpath(Pkg.dir("MathProgBase"),"test","linproginterface.jl"))
+include(joinpath(math_prog_base_test_dir, "linproginterface.jl"))
 linprogsolvertest(solver)
 
-include(joinpath(Pkg.dir("MathProgBase"),"test","conicinterface.jl"))
+include(joinpath(math_prog_base_test_dir, "conicinterface.jl"))
 coniclineartest(CplexSolver(CPX_PARAM_SCRIND=0))
 conicSOCtest(CplexSolver(CPX_PARAM_SCRIND=0))
 conicSOCRotatedtest(CplexSolver(CPX_PARAM_SCRIND=0))
