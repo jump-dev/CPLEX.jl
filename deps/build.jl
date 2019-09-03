77a0f6d4afa128f946589e73dfc00dff259dbd4f
Thibaut Cuvelier <dourouc05@gmail.com>
2019-09-03 18:19:55 +0200

    More search possibilities for macOS.

--- deps/build.jl
+++ deps/build.jl
@@ -40,16 +40,16 @@ base_cpxvers = ["128", "129"]
 cpxvers = [base_cpxvers; base_cpxvers .* "0"]
 
 libnames = String["cplex"]
-for v in reverse(cpxvers)
-    if Sys.isapple()
-        push!(libnames, "libcplex$v.dylib")
-    elseif Sys.isunix()
-        push!(libnames, "libcplex$v.so")
+
+@static if ! Sys.iswindows()
+    extension = if Sys.isapple(); ".dylib"; else; ".so"; end
+    for v in reverse(cpxvers)
+        push!(libnames, "libcplex$(v).$(extension)")
         if haskey(ENV, base_env)
-            push!(libnames, joinpath(ENV[base_env], "libcplex$v.so"))
+            push!(libnames, joinpath(ENV[base_env], "libcplex$(v).$(extension)"))
         end
         if cplex_path !== nothing
-            push!(libnames, joinpath(cplex_path, "libcplex$v.so"))
+            push!(libnames, joinpath(cplex_path, "libcplex$(v).$(extension)"))
         end
     end
 end
