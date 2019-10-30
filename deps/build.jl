b33e504888e7964baf2f2e77efc3f3577051e17a
Oscar Dowson <odow@users.noreply.github.com>
2019-10-30 11:01:30 -0500

    Test CPLEX on travis (#258)
    * Test CPLEX on travis
    
    * Fix build script
    
    * Try encrypting the secret again
    
    * Use web interface instead

--- deps/build.jl
+++ deps/build.jl
@@ -1,74 +1,84 @@
 using Libdl
 
-depsfile = joinpath(dirname(@__FILE__), "deps.jl")
+const depsfile = joinpath(dirname(@__FILE__), "deps.jl")
 if isfile(depsfile)
     rm(depsfile)
 end
 
 function write_depsfile(path)
     open(depsfile, "w") do f
-        print(f, "const libcplex = ")
-        show(f, path) # print with backslashes excaped on windows
-        println(f)
+        println(f, "const libcplex = \"$(escape_string(path))\"")
     end
+end
+
+function library_name(v)
+    cpx_prefix = Sys.iswindows() ? "" : "lib"
+    return "$(cpx_prefix)cplex$(v).$(Libdl.dlext)"
 end
 
 @static if Sys.isapple()
     Libdl.dlopen("libstdc++", Libdl.RTLD_GLOBAL)
 end
 
-base_cpxvers = ["128", "129"]
-cpxvers = [base_cpxvers; base_cpxvers .* "0"]
-base_env = "CPLEX_STUDIO_BINARIES"
-
-# Find the path to the CPLEX executable.
-cplex_path = try
-    @static if Sys.isapple() || Sys.isunix()
-        read(`which cplex`, String)
-    elseif Sys.iswindows()
-        read(`where cplex`, String)
-    end
-catch
-    nothing
-end
-if cplex_path !== nothing
-    # Extract the path to the folder containing the CPLEX executable.
-    cplex_path = dirname(strip(cplex_path))
-end
-
-# Iterate through a series of places where CPLEX could be found: either in the path (directly the callable library or
-# the CPLEX executable) or from an environment variable.
-cpx_prefix = Sys.iswindows() ? "" : "lib"
-
-libnames = String["cplex"]
-for v in reverse(cpxvers)
-    push!(libnames, "$(cpx_prefix)cplex$(v).$(Libdl.dlext)")
-    if cplex_path !== nothing
-        push!(libnames, joinpath(cplex_path, "$(cpx_prefix)cplex$(v).$(Libdl.dlext)"))
+function try_local_installation()
+    # Find the path to the CPLEX executable.
+    cplex_path = try
+        @static if Sys.isapple() || Sys.isunix()
+            dirname(strip(read(`which cplex`, String)))
+        elseif Sys.iswindows()
+            dirname(strip(read(`where cplex`, String)))
+        end
+    catch
+        nothing
     end
 
-    for env in [base_env, base_env * v]
-        if haskey(ENV, env)
+    # Iterate through a series of places where CPLEX could be found: either in
+    # the path (directly the callable library or # the CPLEX executable) or from
+    # an environment variable.
+    cpxvers = [
+        "128", "1280", "129", "1290"
+    ]
+    base_env = "CPLEX_STUDIO_BINARIES"
+
+    libnames = String["cplex"]
+    for v in reverse(cpxvers)
+        name = library_name(v)
+        push!(libnames, name)
+        if cplex_path !== nothing
+            push!(libnames, joinpath(cplex_path, name))
+        end
+        for env in [base_env, base_env * v]
+            !haskey(ENV, env) && continue
             for d in split(ENV[env], ';')
-                occursin("cplex", d) || continue
-                push!(libnames, joinpath(d, "$(cpx_prefix)cplex$(v).$(Libdl.dlext)"))
+                !occursin("cplex", d) && continue
+                push!(libnames, joinpath(d, name))
             end
         end
     end
-end
 
-# Perform the actual search in the potential places.
-found = false
-for l in libnames
-    d = Libdl.dlopen_e(l)
-    if d != C_NULL
-        global found = true
+    # Perform the actual search in the potential places.
+    for l in libnames
+        d = Libdl.dlopen_e(l)
+        d == C_NULL && continue
         write_depsfile(Libdl.dlpath(d))
-        @info("Using CPLEX found in location `$(libname)`")
-        break
+        @info("Using CPLEX found in location `$(l)`")
+        return
     end
+    error(
+        "Unable to locate CPLEX installation. Note this must be downloaded " *
+        "separately. See the CPLEX.jl README for further instructions."
+    )
 end
 
-if !found
-    error("Unable to locate CPLEX installation. Note this must be downloaded separately. See the CPLEX.jl README for further instructions.")
+function try_travis_installation()
+    url = ENV["SECRET_CPLEX_URL"]
+    local_filename = joinpath(@__DIR__, "libcplex.so")
+    download(url, local_filename)
+    write_depsfile(local_filename)
+end
+
+if get(ENV, "TRAVIS", "false") == "true"
+    try_travis_installation()
+else
+    try_local_installation()
 end
