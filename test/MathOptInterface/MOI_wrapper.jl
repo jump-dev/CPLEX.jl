42aecc2e7106e7433f2140251885bbd7964814b9
Oscar Dowson <odow@users.noreply.github.com>
2022-03-09 15:44:57 +1300

    Fix typo in get for RawOptimizerAttribute (#389)
    * Fix typo in get for RawOptimizerAttribute
    
    * Update MOI_wrapper.jl
    
    * Update MOI_wrapper.jl

--- test/MathOptInterface/MOI_wrapper.jl
+++ test/MathOptInterface/MOI_wrapper.jl
@@ -198,6 +198,14 @@ function test_fake_status()
     return
 end
 
+function test_getlongparam()
+    model = CPLEX.Optimizer()
+    y = MOI.get(model, MOI.RawOptimizerAttribute("CPX_PARAM_INTSOLLIM"))
+    # The default is a really big number, but not the typemax.
+    @test y > 0.95 * typemax(y)
+    return
+end
+
 end  # module TestMOIwrapper
 
 TestMOIwrapper.runtests()
